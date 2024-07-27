0f9898c063fa2fac025b628213086007c97121da
bhoomikshetty <shettybhoomik@gmail.com>
2024-07-27 20:20:42 +0530

    socket working

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,17 +2,21 @@ import http, { Server } from 'http'
 import SocketService from './services/socket'
 
 async function init() {
-    const httpServer = http.createServer()
-    const PORT = process.env.PORT ?? 3000
-
     const socketService = new SocketService()
-    socketService.io.attach(httpServer)
 
-    httpServer.listen(() => {
+    const httpServer = http.createServer()
+    const PORT = process.env.PORT ?? 8000
+
+    socketService.io.attach(httpServer)
+    try {
+    } catch (error) {
+        console.log(error)
+    }
+    httpServer.listen(PORT, () => {
         console.log(`http server started at ${PORT}`)
     })
 
     socketService.initListeners()
 }
 
-init()
+init();
